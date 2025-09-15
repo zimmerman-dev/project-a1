2687bbdf52b47b51d490f733bc108ea3c2e152fe
John Zimmerman <john@meetjohnz.com>
2025-09-14 19:06:38 -0500

    test: testing zed git gui

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
+
+void greeter() {
+    std::cout << "Enter your first name: ";
+    std::string name{};
+    std::cin >> name;
+    std::cout << "Welcome " << name << ".\n";
+}
 
 int main() {
-
-    std::cout << "Hello1" << std::endl;
+    greeter();
+    std::cout << "This is project-a1!" << '\n';
 
     return 0;
 }
